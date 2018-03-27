804393079d66cad282622235c7879681493f3ba1
Joel Sequeira <joen.sg@gmail.com>
2018-03-27 12:21:22 +0100

    finish crud_spec rq01

--- module1-solution/1/raceday/app/models/racer.rb
+++ module1-solution/1/raceday/app/models/racer.rb
@@ -1,8 +1,24 @@
 class Racer
   include Mongoid::Document
 
-  def mongo_client
-    Mongoid::Clients.default
+  @@db = nil
+
+  def self.all(prototype = {}, sort = {:number => 1}, skip = 0, limit = nil)
+    result = self.collection.find(prototype).sort(sort).skip(skip)
+    result = result.limit(limit) unless limit.nil?
+    return result
+  end
+
+  # a class method that returns a MongoDB Client configured to communicate to the default database
+  # as specified in the config/mongoid.yml file
+  def self.mongo_client
+    @@db = Mongoid::Clients.default
+  end
+
+  # a class method that returns the racers MongoDB Collection object
+  def self.collection
+    self.mongo_client unless @@db
+    @@db[:racers]
   end
 
 end
